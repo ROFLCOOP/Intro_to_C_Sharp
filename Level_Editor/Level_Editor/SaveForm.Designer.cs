8fea48c0d7d4967cb0808fe4b7bfe72644417e19
Unknown <andrewckenny97@gmail.com>
2018-10-22 16:50:07 +1100

    Added Save Form

---
+++ Level_Editor/Level_Editor/SaveForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Level_Editor
+{
+	partial class SaveForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonSave = new System.Windows.Forms.Button();
+			this.buttonCancel = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.labelDescription = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// buttonSave
+			// 
+			this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonSave.Location = new System.Drawing.Point(12, 68);
+			this.buttonSave.Name = "buttonSave";
+			this.buttonSave.Size = new System.Drawing.Size(75, 30);
+			this.buttonSave.TabIndex = 0;
+			this.buttonSave.Text = "Save";
+			this.buttonSave.UseVisualStyleBackColor = true;
+			// 
+			// buttonCancel
+			// 
+			this.buttonCancel.Location = new System.Drawing.Point(115, 68);
+			this.buttonCancel.Name = "buttonCancel";
+			this.buttonCancel.Size = new System.Drawing.Size(75, 30);
+			this.buttonCancel.TabIndex = 1;
+			this.buttonCancel.Text = "Cancel";
+			this.buttonCancel.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(16, 42);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(128, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// labelDescription
+			// 
+			this.labelDescription.AutoSize = true;
+			this.labelDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelDescription.Location = new System.Drawing.Point(13, 13);
+			this.labelDescription.Name = "labelDescription";
+			this.labelDescription.Size = new System.Drawing.Size(171, 26);
+			this.labelDescription.TabIndex = 3;
+			this.labelDescription.Text = "What would you like to name\r\n your save?";
+			// 
+			// SaveForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(202, 111);
+			this.Controls.Add(this.labelDescription);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.buttonCancel);
+			this.Controls.Add(this.buttonSave);
+			this.Name = "SaveForm";
+			this.Text = "SaveForm";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonSave;
+		private System.Windows.Forms.Button buttonCancel;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label labelDescription;
+	}
+}
